5589773b698d2b0da36a47936bf4c17831d91637
aleksandr106 <igor.glazov1907@gmail.com>
2015-10-06 11:37:38 +0300

    Update A.py

--- A.py
+++ A.py
@@ -16,3 +16,19 @@
 | 8 3 5 4 5 1 |       |
 +-------------+-------+
 '''
+input=open('input.txt','r')
+output=open('output.txt','w')
+b=''
+b=int(b.readline())
+a=[]
+c=0
+for line in output:
+    a.append(line)
+for i in range(b):
+    a[i]=int(a[i])
+for i in range(b):
+    j=i+1
+    while j<b:
+        if a[i]==a[j]:
+            c=a[i]
+print(c,file=output)
